42e8d3f82946d0212771368de10fa2a2d97d5c88
BUKOLA OMOTOSO <bukola_omotoso@BUKOLAs-MacBook-Pro.local>
2016-06-01 16:08:40 +0100

    An app to make users get acquainted with flags of various countries

---
+++ app/src/main/java/com/example/bukola_omotoso/flagquiz/SettingsActivityFragment.java
@@ -0,0 +1,23 @@
+package com.example.bukola_omotoso.flagquiz;
+
+import android.preference.PreferenceFragment;
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * A placeholder fragment containing a simple view.
+ */
+public class SettingsActivityFragment extends PreferenceFragment {
+
+    public SettingsActivityFragment() {
+    }
+
+    @Override
+    public void onCreate(Bundle bundle) {
+        super.onCreate(bundle);
+        addPreferencesFromResource(R.xml.preferences);
+    }
+}
